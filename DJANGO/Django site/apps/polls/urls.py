8ef8839e7aeca67fca7da83d2b630d861561a5a3
E Bsnt <the.bsnt.official@gmail.com>
2025-02-13 15:15:55 +0545

    handling post request, http_response_redirect, PRG pattern, reverse URL resolution

--- DJANGO/Django site/apps/polls/urls.py
+++ DJANGO/Django site/apps/polls/urls.py
@@ -6,5 +6,6 @@ app_name = "polls"
 urlpatterns = [
     path("", index, name="index"),
     path("<int:question_id>/", detail, name="detail"),
-    path("<int:question_id>/", vote, name="vote"),
+    path("<int:question_id>/results/", results, name="results"),
+    path("<int:question_id>/vote/", vote, name="vote"),
 ]
